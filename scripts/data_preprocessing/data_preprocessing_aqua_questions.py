b7e27d67b11e41d32bf82b00d77593cc1763a305
Albert <albert.kaloustian@gmail.com>
2024-10-13 20:47:15 -0700

    Updates to python script and new jupyter notebook

--- scripts/data_preprocessing/data_preprocessing_aqua_questions.py
+++ scripts/data_preprocessing/data_preprocessing_aqua_questions.py
@@ -3,11 +3,16 @@ from transformers import T5Tokenizer, AutoTokenizer
 import pandas as pd
 import torch
 import os
+from tqdm import tqdm
 
 # Load tokenizers
 t5_tokenizer = T5Tokenizer.from_pretrained('google-t5/t5-small')
 llemma_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/llemma_7b")
 gptneo_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+
+# Response word limit
+word_limit = 200
+max_token_length = 256
 
 teacher_tokenizers = {
     'llemma': llemma_tokenizer,
@@ -29,14 +34,21 @@ def load_aqua_data():
 
 
 # Function to preprocess data
-def preprocess_data_for_models(df, max_length=256):
+def preprocess_data_for_models(df, max_length=max_token_length):
     teacher_inputs = {key: [] for key in teacher_tokenizers.keys()}
     student_inputs = []
 
-    for index, row in df.iterrows():
-        question_text = f"Question: {row['question']} Options: {row['options']}. Explain your rationale, and limit your total response to 200 words max."
+    for index, row in tqdm(df.iterrows(), total = len(df), desc="Processing Data"):
+        question_text = f"Question: {row['question']} Options: {row['options']}. Explain your rationale in reaching the answer, and limit your rationale to {word_limit} words. Provide your response in the following JSON format:\n" 
+        f'{{"answer": "Your answer here", "rationale": "Your rationale here"}}'
+        
 
-        student_question_encoding = t5_tokenizer(question_text, return_tensors='pt', padding='max_length', truncation=True, max_length=max_length)
+        student_question_encoding = t5_tokenizer(
+            question_text, 
+            return_tensors='pt', 
+            padding='max_token_length', 
+            truncation=True, 
+            max_length=max_token_length)
 
         # Student tokens
         student_inputs.append({
@@ -48,10 +60,18 @@ def preprocess_data_for_models(df, max_length=256):
             if tokenizer.pad_token is None:
                 tokenizer.pad_token = tokenizer.eos_token
 
-            question_encoding = tokenizer(question_text, return_tensors='pt', padding='max_length', truncation=True, max_length=max_length)
+            question_encoding = tokenizer(
+                question_text, 
+                return_tensors='pt', 
+                padding='max_token_length', 
+                truncation=True, 
+                max_length=max_token_length
+            )
             assert question_encoding['input_ids'].shape == question_encoding['attention_mask'].shape
             
-            teacher_inputs[teacher_name].append({'input_ids': question_encoding['input_ids'], 'attention_mask': yquestion_encoding['attention_mask'],
+            teacher_inputs[teacher_name].append({
+                'input_ids': question_encoding['input_ids'], 
+                'attention_mask': question_encoding['attention_mask'],
             })
 
     return teacher_inputs, student_inputs
@@ -81,10 +101,12 @@ def save_preprocessed_data(preprocessed_data, filename):
         os.makedirs(directory)
     filepath = os.path.join(directory, filename)
     torch.save(preprocessed_data, filepath)
-    print(f"Preprocessed data saved to {filename}")
+    print(f"Preprocessed data saved to {filepath}")
 
 
 if __name__ == '__main__':
     preprocessed_data = get_preprocessed_data()
     save_preprocessed_data(preprocessed_data, 'preprocessed_aqua_data.pt')
-    print("Train Teacher Input Example:", preprocessed_data['train']['teacher_inputs']['llemma'][0])
+    example_input_ids = preprocessed_data['train']['teacher_inputs']['llemma'][0]['input_ids']
+    decoded_text = llemma_tokenizer.decode(example_input_ids.squeeze(), skip_special_tokens=True)
+    print("Train Teacher Input Example: ", decoded_text)
